3672671f45868ee99f36bfaa6b7d583523e73c13
Carlos Centeno <136040843+herocar@users.noreply.github.com>
2026-06-12 16:31:51 -0500

    Usar almacenamiento en memoria para Render

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from models import ItemPayload
 
 app = FastAPI()
 
-import os
-
 redis_client = redis.StrictRedis(
-    host=os.getenv("REDIS_HOST", "localhost"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
+    host="localhost",
+    port=6379,
     db=0,
     decode_responses=True
 )
